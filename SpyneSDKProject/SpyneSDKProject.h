a8192324e157e878c7fbdd63c0aa4172f15c3023
Akash Verma <akash.verma@spyne.ai>
2022-06-06 17:24:41 +0530

    Initial Commit

---
+++ SpyneSDKProject/SpyneSDKProject.h
@@ -0,0 +1,18 @@
+//
+//  SpyneSDKProject.h
+//  SpyneSDKProject
+//
+//  Created by Admin on 06/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpyneSDKProject.
+FOUNDATION_EXPORT double SpyneSDKProjectVersionNumber;
+
+//! Project version string for SpyneSDKProject.
+FOUNDATION_EXPORT const unsigned char SpyneSDKProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpyneSDKProject/PublicHeader.h>
+
+
